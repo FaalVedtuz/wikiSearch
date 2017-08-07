fa9f32b47e4cc3247c8ce97a7e877bd80b68a4c3
FaalVedtuz <jmr_mymsaccnt@outlook.com>
2017-08-06 23:19:18 -0700

    added some search functionalities

--- wikisearch.js
+++ wikisearch.js
@@ -1,31 +1,49 @@
-$(function(){
-  var wikiQ;
+$(function() {
+    var wikiQ;
 
-function fetchData(query){
-  $.ajax({
-    url: "https://en.wikipedia.org/w/api.php",
-    dataType: 'jsonp',
-    type: "POST",
-    data:{
-      action: 'opensearch',
-      format: 'json',
-      search: query
-    },
-    success: function(data){
-      console.log(wikiQ);
-      console.log("data fetched.." + data);
-    },
-    error: function(req,errStat,errMsg){
-      console.log("Sorry there has been a problem. Error: " + errStat + req);
+    function fetchData(query) {
+        $.ajax({
+            url: "https://en.wikipedia.org/w/api.php",
+            dataType: 'jsonp',
+            type: "POST",
+            data: {
+                action: 'opensearch',
+                format: 'json',
+                limit: 5,
+                search: query
+            },
+            success: function(data) {
+                console.log(data[1]);
+                clearPartialResult();
+                if (wikiQ !== "") {
+                    for (var i = 0; i < data[1].length; i++) {
+                        $("#partial_results").append("<p><a href=" + data[3][i] + ">" + data[1][i] + "</a></p>");
+                    }
+                    $("#partial_results").fadeIn("fast");
+                } else {
+                    $("#partial_results").hide();
+                }
+            },
+            error: function(req, errStat, errMsg) {
+                console.log("Sorry there has been a problem. Error: " + errStat + req);
+            }
+        });
+    };
+
+    function clearPartialResult() {
+        $("#partial_results p").remove();
     }
-  });
-};
+    //remove suggestion box when click outside of the suggestion container
+    $("body").click(function(e) {
+        $("#partial_results").fadeOut("fast");
+        e.stopPropagation();
+    });
 
-  $("#search").keypress(function(e){
-    wikiQ = $("#search").val();
-    //console.log("typing.." + wikiQ);
-    fetchData(wikiQ);
-  });
+    $("#searchForm").keyup(function(e) {
+        //e.preventDefault();
+        wikiQ = $("#search").val();
+        fetchData(wikiQ);
+    });
 
 
 }); // document.ready end
